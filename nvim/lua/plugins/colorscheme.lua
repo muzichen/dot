7afaf4d2a409052c754ba8a58411a535c672e990
ChenLee <lichen7@hetao101.com>
2024-12-17 16:07:10 +0800

    delete unused files

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,48 @@
+-- return {
+--   "folke/tokyonight.nvim",
+--   lazy = false,
+--   priority = 1000,
+--   opts = {
+--     style = "night",
+--     transparent = true
+--   },
+-- }
+-- return {
+--   { "scottmckendry/cyberdream.nvim" },
+--
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       transparent = true,
+--       lazy = false,
+--       colorscheme = "cyberdream",
+--     },
+--   }
+-- }
 return {
-  { "scottmckendry/cyberdream.nvim" },
-
   {
-    "LazyVim/LazyVim",
-    opts = {
-      transparent = true,
-      lazy = false,
-      colorscheme = "cyberdream",
-    },
-  }
+    "craftzdog/solarized-osaka.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = function()
+      return {
+        transparent = true,
+      }
+    end,
+    config = function()
+      vim.cmd("colorscheme solarized-osaka")
+    end,
+  },
 }
+
+-- return {
+--   "ellisonleao/gruvbox.nvim",
+--   priority = 1000,
+--   opts = {
+--     contrast = "hard",
+--     transparent_mode = true,
+--   },
+--   config = function()
+--     vim.cmd("colorscheme gruvbox")
+--   end,
+-- }
